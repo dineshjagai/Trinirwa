ff29b1ba072c09ec072ac82a737377756b4b13f1
Isimbi Bizimana <isimbib@seas.upenn.edu>
2021-03-20 16:02:21 -0400

    Create login.test.js

---
+++ login.test.js
@@ -0,0 +1,37 @@
+const { test } = require('@jest/globals');
+const log = require('./login');
+
+
+
+// Valid credentials
+test('validating username: no username entered', ()=> { 
+    expect(log.isValidUsername("")).toBe(false);
+});
+
+test('validating password: no password entered', ()=> { 
+    expect(log.isValidPassWord("")).toBe(false);
+});
+
+test('validating password: wrong pass', ()=> { 
+    expect(log.isValidPassWord("Rasabe")).toBe(false);
+});
+
+test('validating username: wrong username', ()=> { 
+    expect(log.isValidUsername("ana")).toBe(false);
+});
+
+test('validating password: correct pass', ()=> { 
+    expect(log.isValidPassWord("!Dragon@907")).toBe(true);
+});
+
+
+test('validating username: correct username', ()=> { 
+    expect(log.isValidUsername("anaick")).toBe(true);
+});
+
+// Go to login page
+test('validating user :', ()=> { 
+    expect(log.userExists("anaick", "!Dragon@907")).toBe(true);
+});
+
+
