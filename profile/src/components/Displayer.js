4e3cf443ffaabcd5f8b774e85637bf86b7c1329f
Arnaud3 <61192534+Arnaud3@users.noreply.github.com>
2021-04-12 11:26:04 -0400

    updates

--- profile/src/components/Displayer.js
+++ profile/src/components/Displayer.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
 import Divider from '@material-ui/core/Divider';
 import DialogBox from './Dialog.js'
+
 export default function Displayer(props){
     const[interests, setInterests]= useState(props.interests);
     function handleDelete(interes){
@@ -19,6 +20,7 @@ export default function Displayer(props){
         const Newinterests = interests.filter(interest=> interest != interes);
         setInterests(Newinterests);
     }
+
     const items = interests.map((interest)=>
         <li  onClick= {()=>handleDelete(interest)} className= "badge rounded-pill bg-secondary m-2" key={interest}>
             {interest}
@@ -30,10 +32,9 @@ export default function Displayer(props){
             <div className="section1">
                 <ul >{items}</ul>
             </div>
-            
             <Divider  variant="middle"/>
             <div className="section2">
-                <DialogBox onDelete id={props.id}/>
+                <DialogBox  id={props.id}/>
             </div>
         </div>
         
