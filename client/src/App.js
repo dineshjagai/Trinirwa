0a0716822f1a27f191dfdfd9178876691126c847
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-05-06 17:40:09 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,49 @@ import Home from './components/Home';
 import { userLogin, getUid } from './components/Module';
 
 function App() {
+  const [uid, setUid] = useState('');
+  const [count, setCount] = useState(0);
+  Axios.defaults.withCredentials = true;
+
+  // useEffect(() => setUid(uid), [uid, count]);
+  useEffect(() => console.log(`changed uid ${uid}`), [uid]);
+  // useEffect(() => {
+  //   if (true) {
+  //     window.history.replaceState({ uid }, 'new page', '/home');
+  //     window.location.reload();
+  //   }
+  // }, [uid]);
+
+  const handelLoginUid = (username, password) => {
+    userLogin(username, password).then((response) => {
+      if (response.data.message) {
+        alert(response.data.message);
+        window.history.replaceState(null, 'new page', '/login');
+        window.location.reload();
+        // setUsername('');
+        // setPassword('');
+        setUid('');
+        setCount(count + 1);
+        if (count >= 3) {
+          // disable user from logging into the browser
+          // TODO implement this
+        }
+        document.getElementById('new-password-form').value = '';
+        document.getElementById('new-username-form').value = '';
+      } else {
+        let fetchedUid = null;
+        getUid(username).then((res) => {
+          [fetchedUid] = [Array.from(res.data.data)[0].uid];
+          console.log('fetched UID = ');
+          console.log(fetchedUid);
+          setUid(fetchedUid);
+        }).catch((e) => {
+          console.log(e);
+        });
+      }
+    });
+  };
+  console.log(count);
   return (
     <Router>
       <Route path="/" exact render={() => <Registration />} />
