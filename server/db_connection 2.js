58ecade0123852cb0356f4d427e01f9f12a37aad
Arnaud3 <61192534+Arnaud3@users.noreply.github.com>
2021-05-09 00:03:17 -0400

    Delete db_connection 2.js

--- server/db_connection 2.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  host: 'database-1.cmkrry719kkl.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'XQJYs10aUBLqQm5R2Skq',
-  database: 'TRINIWA',
-};
-
-module.exports = config;
