ba8e7d3576fa5e226fd451895456f005f6592284
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-05-11 20:18:08 -0400

    Video chat almost done

---
+++ client/src/live_stream/videoStreamComponents.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoChat from './VideoChat';
+import './videoStreamComponents.css';
+
+export default function VideoStreamComponent() {
+  return (
+    <main>
+      <VideoChat />
+    </main>
+
+  );
+}
