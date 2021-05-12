16acb370738306670826e869859f5d3e487fe487
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-05-12 13:44:56 -0400

    edit travis

---
+++ server/login.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+const { test } = require('@jest/globals');
+const log = require('./authentication');
+
+// Valid credentials
+test('validating username: no username entered', () => {
+  expect(log.isValidUsername('')).toBe(false);
+});
+
+test('validating password: no password entered', () => {
+  expect(log.isValidPassWord('')).toBe(false);
+});
+
+test('validating password: wrong pass', () => {
+  expect(log.isValidPassWord('Rasabe')).toBe(false);
+});
+
+test('validating username: wrong username', () => {
+  expect(log.isValidUsername('ana')).toBe(false);
+});
+
+test('validating password: correct pass', () => {
+  expect(log.isValidPassWord('!Dragon@907')).toBe(true);
+});
+
+test('validating username: correct username', () => {
+  expect(log.isValidUsername('anaick')).toBe(true);
+});
