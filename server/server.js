72fb35ba1ac02cd557359703d183c3ff0c05639e
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-05-12 15:30:23 -0400

    fixed login

--- server/server.js
+++ server/server.js
@@ -189,7 +189,7 @@ webapp.post('/login', (req, res) => {
           if (response) {
             req.session.user = result;
             console.log(req.session.user);
-            res.send({ message: 'success', result });
+            res.send(result);
           } else {
             console.log(`here ${result}`);
             res.send({ message: 'Wrong username/password combination!' });
@@ -522,7 +522,7 @@ webapp.put('/profile/reactivate/:username', (req, res) => {
       bcrypt.compare(password, result[0].password, (error, response) => {
         // console.log(response);
         if (error) {
-          res.status(401).json({ error: error });
+          res.status(401).json({ error });
         } else if (response) {
           connection.query(sql_deact, user,
             function (errr) {
