1efbb56cb3fdfc78b4848faa8d5eb44fb4a260ac
Isimbi Bizimana <isimbib@seas.upenn.edu>
2021-05-14 22:15:00 -0400

    new tests to do

--- client/src/tests/Tweet.test.js
+++ client/src/tests/Tweet.test.js
@@ -9,11 +9,19 @@ test('Test Tweet snapshot', () => {
     'tweet_date' :  '2008-09-07',
     'content' : 'Hiii',
     'tweet_likes': 0,
-    'tweet_id' :  1,
-    'type' : 'text',
+    'tweet_id' :  '1',
+    'type' : 'picture',
     'tweet_blocks': 0,
   };
-  const component = renderer.create(<Tweet data={d}/>);
+
+  const handle = (id, owner) => {
+    if (owner) {
+      console.log("owner");
+    } else {
+      console.log("HERE {id}");
+    }
+  };
+  const component = renderer.create(<Tweet data={d} handleDelete={handle}/>);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
