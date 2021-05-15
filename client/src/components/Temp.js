c0c988ea94448095d3a36a66770167ca84980983
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-05-15 12:16:43 -0400

    fixed deployment issues

--- client/src/components/Temp.js
+++ client/src/components/Temp.js
@@ -4,7 +4,7 @@ import '../App.css';
 export default function Temp() {
   return (
     <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-      <img src="/viewFile/cat1.jpg" alt="" className="img-responsive" />
+      <img src="/api/viewFile/cat1.jpg" alt="" className="img-responsive" />
     </div>
 
   );
